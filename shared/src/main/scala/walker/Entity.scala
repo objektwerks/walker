68c4a06897fc3244624f5da280776244e2053c73
objektwerks <objektwerks@runbox.com>
2023-11-07 13:12:22 -0500

    edited distance unit

--- shared/src/main/scala/walker/Entity.scala
+++ shared/src/main/scala/walker/Entity.scala
@@ -46,47 +46,36 @@ final case class Session(id: Long = 0,
                          walkerId: Long,
                          weight: Int = 150,
                          weightUnit: String = WeightUnit.lb.toString,
-                         laps: Int = 10,
-                         lapDistance: Int = 50,
-                         lapUnit: String = LapUnit.yards.toString,
-                         style: String = Style.freestyle.toString,
-                         kickboard: Boolean = false,
-                         fins: Boolean = false,
-                         minutes: Int = 15,
-                         seconds: Int = 0,
+                         distance: Double = 2,
+                         distanceUnit: String = DistanceUnit.m.toString,
+                         hours: Int = 0,
+                         minutes: Int = 30,
                          calories: Int = 150,
                          datetime: Long = Instant.now.toEpochMilli) extends Entity:
   val weightProperty = ObjectProperty[Double](this, "weight", weight)
   val weightUnitProperty = ObjectProperty[String](this, "weightUnit", weightUnit)
-  val lapsProperty = ObjectProperty[Int](this, "laps", laps)
-  val lapDistanceProperty = ObjectProperty[Int](this, "lapDistance", laps)
-  val lapUnitProperty = ObjectProperty[String](this, "lapUnit", lapUnit)
-  val styleProperty = ObjectProperty[String](this, "style", style)
-  val kickboardProperty = ObjectProperty[Boolean](this, "kickboard", kickboard)
-  val finsProperty = ObjectProperty[Boolean](this, "fins", fins)
+  val distanceProperty = ObjectProperty[Double](this, "distance", distance)
+  val distanceUnitProperty = ObjectProperty[String](this, "distanceUnit", distanceUnit)
+  val hoursProperty = ObjectProperty[Int](this, "hours", hours)
   val minutesProperty = ObjectProperty[Int](this, "minutes", minutes)
-  val secondsProperty = ObjectProperty[Int](this, "seconds", seconds)
   val caloriesProperty = ObjectProperty[Int](this, "calories", calories)
   val datetimeProperty = ObjectProperty[String](this, "datetime", Entity.format(datetime))
   val session = this
 
-  def roundSecondsToMinute(): Int = if seconds > 29 then 1 else 0
+  def calculateMinutes(): Int = (hours * 60) + minutes
 
   def caloriesBurned(): Int =
     if weight < 1.0 || minutes < 1 then 0
     else
       val kg = if WeightUnit.lb.toString == weightUnit then WeightUnit.lbsToKgs(weight) else weight.toDouble
       val met = (Session.MET * 3.5 * kg) / 200
-      val mins = minutes + roundSecondsToMinute()
+      val mins = calculateMinutes()
       ( mins * met ).round.toInt
-
-  def distance(): Int = laps * lapDistance
-  def speed(): Int = distance() / ( minutes + roundSecondsToMinute() )
 
 object Session:
   val MET = 6
   given JsonValueCodec[Session] = JsonCodecMaker.make[Session]
-  given sessionOrdering: Ordering[Session] = Ordering.by[Session, Long](dt => dt.datetime).reverse
+  given sessionOrdering: Ordering[Session] = Ordering.by[Session, Long](session => session.datetime).reverse
 
 enum WeightUnit:
   case lb, kg
@@ -96,14 +85,8 @@ object WeightUnit:
   def kgsToLbs(kgs: Int): Double = kgs * 2.205
   def toList: List[String] = WeightUnit.values.map(wu => wu.toString).toList
 
-enum LapUnit:
-  case feet, meters, yards
+enum DistanceUnit:
+  case km, mi
 
-object LapUnit:
-  def toList: List[String] = LapUnit.values.map(lu => lu.toString).toList
-
-enum Style:
-  case breaststroke, backstroke, butterfly, freestyle, kick
-
-object Style:
-  def toList: List[String] = Style.values.map(s => s.toString).toList
+object DistanceUnit:
+  def toList: List[String] = DistanceUnit.values.map(du => du.toString).toList
