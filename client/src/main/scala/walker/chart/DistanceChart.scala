fdb1218eefc0f11f6767f0448df64dd720895c21
objektwerks <objektwerks@runbox.com>
2023-11-07 13:29:31 -0500

    adapted distance chart to new session

--- client/src/main/scala/walker/chart/DistanceChart.scala
+++ client/src/main/scala/walker/chart/DistanceChart.scala
@@ -9,7 +9,7 @@ import scalafx.scene.chart.{LineChart, XYChart}
 import scalafx.scene.control.{ComboBox, Separator, Tab}
 import scalafx.scene.layout.{Region, VBox}
 
-import walker.{Context, Entity, Model, Style}
+import walker.{Context, Entity, Model}
 import walker.dialog.ControlGridPane
 
 final case class DistanceXY(xDate: String, yCount: Int)
@@ -20,40 +20,26 @@ final class DistanceChart(context: Context, model: Model) extends Tab:
   val minDate = Entity.toLocalDateTime( sessions.map(e => e.datetime).min ).format(dateFormat)
   val maxDate = Entity.toLocalDateTime( sessions.map(e => e.datetime).max ).format(dateFormat)
 
-  val styleComboBox = new ComboBox[String]:
-  	items = ObservableBuffer.from( Style.toList )
-  	value = Style.freestyle.toString
-  styleComboBox.prefWidth = 300
-  styleComboBox.onAction = { _ => buildChart( Style.valueOf(styleComboBox.value.value) ) }
-
-  val controls = List[(String, Region)](
-    context.labelStyle -> styleComboBox
-  )
-
   closable = false
   text = context.tabDistance
   content = new VBox {
     spacing = 6
     padding = Insets(6)
     children = List(
-      ControlGridPane(controls),
-      Separator(Orientation.Horizontal),
-      buildChart(Style.freestyle)
+      buildChart()
     )
   }
 
-  def buildChart(style: Style): LineChart[String, Number] =
-    val filtered = sessions
-      .filter(s => s.style == style.toString)
-      .map(s => DistanceXY( Entity.toLocalDateTime(s.datetime).format(dateFormat), s.distance()) )
+  def buildChart(): LineChart[String, Number] =
+    val filtered = sessions.map(s => DistanceXY( Entity.toLocalDateTime(s.datetime).format(dateFormat), s.distance.round.toInt) )
     val (chart, series) = LineChartBuilder.build(context = context,
                                                  xLabel = context.chartMonthDay,
                                                  xMinDate = minDate,
                                                  xMaxDate = maxDate,
                                                  yLabel = context.tabDistance,
-                                                 yLowerBound = 100,
-                                                 yUpperBound = 10000,
-                                                 yTickUnit = 100,
+                                                 yLowerBound = 1,
+                                                 yUpperBound = 10,
+                                                 yTickUnit = 1,
                                                  yValues = filtered.map(exy => exy.yCount))
     filtered foreach { exy =>
       series.data() += XYChart.Data[String, Number](exy.xDate.format(dateFormat), exy.yCount)
