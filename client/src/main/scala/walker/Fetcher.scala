46168666b06794df7ea2b02950b4f789ffd954c3
objektwerks <objektwerks@runbox.com>
2023-11-18 16:46:50 -0500

    edited client.conf

--- client/src/main/scala/walker/Fetcher.scala
+++ client/src/main/scala/walker/Fetcher.scala
@@ -3,6 +3,8 @@ package walker
 import com.github.plokhotnyuk.jsoniter_scala.core.*
 import com.typesafe.scalalogging.LazyLogging
 
+import io.helidon.webclient.api.WebClient
+
 import java.net.URI
 import java.net.http.{HttpClient, HttpRequest, HttpResponse}
 import java.net.http.HttpResponse.BodyHandlers
@@ -15,38 +17,28 @@ import scala.concurrent.ExecutionContext
 import scala.jdk.FutureConverters.*
 
 final class Fetcher(context: Context) extends LazyLogging:
-  given ExecutionContext = ExecutionContext.fromExecutor( Executors.newVirtualThreadPerTaskExecutor() )
-  private val uri = URI(context.url)
+  private val url = context.url
   private val connectError = context.errorServer
-  private val client = HttpClient
-                         .newBuilder
-                         .executor( Executors.newVirtualThreadPerTaskExecutor() )
-                         .build
+  private val client = WebClient
+    .builder
+    .baseUri(url)
+    .addHeader("Content-Type", "application/json; charset=UTF-8")
+    .addHeader("Accept", "application/json")
+    .build
 
-  logger.info(s"*** Fetcher url: ${context.url}")
-
-  private def buildHttpRequest(json: String): HttpRequest =
-    HttpRequest
-      .newBuilder
-      .uri(uri)
-      .timeout(Duration.of(30, SECONDS))
-      .version(HttpClient.Version.HTTP_2)
-      .headers("Content-Type", "application/json; charset=UTF-8", "Accept", "application/json")
-      .POST( HttpRequest.BodyPublishers.ofString(json) )
-      .build
+  logger.info(s"*** Fetcher url: $url")
 
   def fetchAsync(command: Command,
                  handler: Event => Unit): Unit =
     logger.info(s"*** Fetcher command: $command")
     val commandJson = writeToString[Command](command)
-    val httpRequest = buildHttpRequest(commandJson)
-    logger.info(s"*** Fetcher http request: $httpRequest")
 
-    client
-      .sendAsync(httpRequest, BodyHandlers.ofString)
-      .asScala
-      .map { httpResponse =>
-        val eventJson = httpResponse.body 
+
+    val eventJson = client
+      .post(endpoint)
+      .submit(commandJson, classOf[String])
+      .entity
+
         val event = readFromString[Event](eventJson)
         logger.info(s"*** Fetcher event: $event")
         Platform.runLater(handler(event))
