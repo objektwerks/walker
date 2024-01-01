c3fc531b5847b3b41519bcfe524544a253a715ef
objektwerks <objektwerks@runbox.com>
2024-01-01 08:54:04 -0500

    refactored double textfield

--- client/src/main/scala/walker/control/DoubleTextField.scala
+++ client/src/main/scala/walker/control/DoubleTextField.scala
@@ -7,17 +7,11 @@ import scalafx.util.converter.DoubleStringConverter
 object DoubleTextField:
   val regex = """([0-9]+([.][0-9]*)?|[.][0-9]+)""".r
 
-class DoubleTextField extends TextField:
+class DoubleTextField extends TextField: 
   val converter = DoubleStringConverter()
   val filter: Change => Change = { (change: Change) =>
-    if DoubleTextField.regex.matches(change.text) then
-      change // if double, make change
-    else
-      if !change.controlText.contains(".") && change.text.contains(".") then
-        change // if . then make change
-      else 
-        change.setText("") // else make no change
-      change
+    if DoubleTextField.regex.matches(change.controlNewText) then change
+    else null
   }
   textFormatter = TextFormatter[Double](converter, 0.0, filter)
 
