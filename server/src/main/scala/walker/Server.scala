54521d343bd7cd313b8861e7940deade63e04105
objektwerks <objektwerks@runbox.com>
2024-04-24 09:10:02 -0400

    enhanced server logging

--- server/src/main/scala/walker/Server.scala
+++ server/src/main/scala/walker/Server.scala
@@ -32,7 +32,7 @@ object Server extends LazyLogging:
       .build
       .start
 
-    println(s"*** Press Control-C to shutdown Walker Http Server at: $host:$port")
-    logger.info(s"*** Walker Http Server started at: $host:$port")
+    println(s"*** Press Control-C to shutdown Walker Http Server at: $host:$port$endpoint")
+    logger.info(s"*** Walker Http Server started at: $host:$port$endpoint")
 
     Thread.currentThread().join()
