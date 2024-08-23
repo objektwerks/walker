92c7960c88d1da315b97ceba6b8d94bab5f705ff
objektwerks <objektwerks@runbox.com>
2024-08-23 12:58:10 -0400

    adapted dispatcher test to ox

--- server/src/test/scala/walker/DispatcherTest.scala
+++ server/src/test/scala/walker/DispatcherTest.scala
@@ -4,6 +4,7 @@ import com.typesafe.config.ConfigFactory
 
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
+import ox.{IO, supervised}
 
 import scala.sys.process.Process
 
@@ -24,21 +25,23 @@ final class DispatcherTest extends AnyFunSuite with Matchers:
   var testSession = Session(walkerId = 0)
 
   test("dispatcher"):
-    register
-    login
+    IO.unsafe:
+      supervised:
+        register
+        login
 
-    deactivate
-    reactivate
+        deactivate
+        reactivate
 
-    addWalker
-    updateWalker
-    listWalkers
+        addWalker
+        updateWalker
+        listWalkers
 
-    addSession
-    updateSession
-    listSessions
+        addSession
+        updateSession
+        listSessions
 
-    fault
+        fault
 
   def register: Unit =
     val register = Register(config.getString("email.sender"))
