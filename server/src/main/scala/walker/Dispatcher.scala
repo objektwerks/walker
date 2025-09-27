319de9f4582ecce0d4d434bc56046dd2867d0f8d
objektwerks <objektwerks@runbox.com>
2025-09-27 10:13:23 -0400

    corrected ox schedule retry

--- server/src/main/scala/walker/Dispatcher.scala
+++ server/src/main/scala/walker/Dispatcher.scala
@@ -122,7 +122,7 @@ final class Dispatcher(store: Store, emailer: Emailer):
   private def saveSession(session: Session): Event =
     try
       SessionSaved(
-        if session.id == 0 then retry( Schedule.fixedInterval(100.millis).maxRepeats(1) )( store.addSession(session) )
+        if session.id == 0 then retry( Schedule.fixedInterval(100.millis).maxAttempts(1) )( store.addSession(session) )
         else retry( Schedule.fixedInterval(100.millis).maxRepeats(1) )( store.updateSession(session) )
       )
     catch
