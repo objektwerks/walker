0f4c5db257ae57a93f14c0690a3710db70dbc801
objektwerks <objektwerks@runbox.com>
2025-09-28 06:56:25 -0400

    edited view split

--- client/src/main/scala/walker/View.scala
+++ client/src/main/scala/walker/View.scala
@@ -23,7 +23,7 @@ final class View(context: Context, model: Model):
     orientation = Orientation.Horizontal
     items.addAll(walkersPane, sessionsPane)
   }
-  splitPane.setDividerPositions(0.30, 0.70)
+  splitPane.setDividerPositions(0.35, 0.65)
   VBox.setVgrow(splitPane, Priority.Always)
 
   vbox.children = List(splitPane)
